3e06694efcd27edc266930033da50187b94f5e49
haklir <r.hakli91@gmail.com>
2018-09-21 23:47:08 +0300

    organize files and code

--- snake_42.py
+++ snake_42.py
@@ -3,30 +3,23 @@
 ## SNAKE 42 ##
 ## -------- ##
 """
-
 import sys
 from random import randint
 from pygame.locals import *
-from pygame.mixer import *
 import pygame as pg
 import snake_data as data
 
 
 def init():
-    pg.init()
+    # pg.init()
     pg.mixer.init()
     pg.mouse.set_cursor((8, 8), (4, 4), (24, 24, 24, 231, 231, 24, 24, 24), (0, 0, 0, 0, 0, 0, 0, 0))
 
 
-init()
 SCREEN = pg.display.set_mode((600, 630))
 STATUS_BAR = Rect(0, 600, 600, 30)
 
-color = GREEN
-
-back = pg.image.load('back.png')  # black 20x20 square, used for resetting
-food = pg.image.load('food.png')
-
+name_input_color = data.GREEN
 play_music = True
 food_is_super = False
 food_position = (300, 300)
@@ -41,97 +34,92 @@ class Game:
         self.width = 600
         self.speed = 6
         self.score = 0
-        self.walls = snake_data.levels[0]
+        self.walls = data.levels[0]
         self.playing = True
         pg.mouse.set_visible(True)
-        SCREEN.fill(BLACK)
+        SCREEN.fill(data.BLACK)
 
     def main(self):
         """ Main menu loop etc. """
 
-        start_box = Rect(250, 200, 100, 35)
-        name_box = Rect(250, 300, 100, 40)
-        green_snake_box = Rect(260, 400, 30, 30)
-        orange_snake_box = Rect(310, 400, 30, 30)
-        music_box = Rect(226, 470, 152, 35)
-        active = False
+        name_input_active = False
         finished = False
-        snake_color = GREEN_SNAKE
-        global name, color, play_music
+        snake_color = data.GREEN_SNAKE
+        global name, name_input_color, play_music
 
-        SCREEN.fill(BLACK)
-        draw_text_box('title:SNAKE 42', Rect(300, 70, 0, 0), RED)
-        draw_text_box('Start', start_box, RED)
-        draw_text_box('small:Enter name', Rect(300, 270, 0, 0), YELLOW)
-        draw_text_box('small:Select color', Rect(300, 370, 0, 0), YELLOW)
-        pg.draw.rect(SCREEN, LIGHTGREEN, green_snake_box)
-        pg.draw.rect(SCREEN, ORANGE, orange_snake_box)
+        SCREEN.fill(data.BLACK)
+        draw_text_box('title:SNAKE 42', Rect(300, 70, 0, 0), data.RED)
+        draw_text_box('Start', data.START_BOX, data.RED)
+        draw_text_box('small:Enter name', Rect(300, 270, 0, 0), data.YELLOW)
+        draw_text_box('small:Select color', Rect(300, 370, 0, 0), data.YELLOW)
+        pg.draw.rect(SCREEN, data.LIGHTGREEN, data.GREEN_SNAKE_BOX)
+        pg.draw.rect(SCREEN, data.ORANGE, data.ORANGE_SNAKE_BOX)
 
         if play_music:
-            draw_text_box('MUSIC off', music_box, RED, GREEN)
+            draw_text_box('MUSIC off', data.MUSIC_BOX, data.RED, data.GREEN)
         else:
-            draw_text_box('MUSIC on', music_box, RED, GREEN)
+            draw_text_box('MUSIC on', data.MUSIC_BOX, data.RED, data.GREEN)
 
-        # logic and loop for name_box and color choosing
+        # logic and loop for data.NAME_BOX and color choosing
         while not finished:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     if wanna_quit():
                         sys.exit()
                 if event.type == pg.MOUSEBUTTONDOWN:
-                    if start_box.collidepoint(event.pos):
+                    if data.START_BOX.collidepoint(event.pos):
                         finished = True
-                    elif music_box.collidepoint(event.pos):
+                    elif data.MUSIC_BOX.collidepoint(event.pos):
                         play_music = not play_music
                         if play_music:
-                            MUSIC.play(-1)
-                            draw_text_box('MUSIC off', music_box, RED, GREEN)
+                            data.MUSIC.play(-1)
+                            draw_text_box('MUSIC off', data.MUSIC_BOX, data.RED, data.GREEN)
                         else:
-                            MUSIC.stop()
-                            draw_text_box('MUSIC on', music_box, RED, GREEN)
-                    if name_box.collidepoint(event.pos):
-                        active = True
-                        color = BLUE
+                            data.MUSIC.stop()
+                            draw_text_box('MUSIC on', data.MUSIC_BOX, data.RED, data.GREEN)
+                    if data.NAME_BOX.collidepoint(event.pos):
+                        name_input_active = True
+                        name_input_color = data.BLUE
                     else:
-                        active = False
-                        color = GREEN
-                    if green_snake_box.collidepoint(event.pos):
-                        snake_color = GREEN_SNAKE
-                    elif orange_snake_box.collidepoint(event.pos):
-                        snake_color = ORANGE_SNAKE
+                        name_input_active = False
+                        name_input_color = data.GREEN
+                    if data.GREEN_SNAKE_BOX.collidepoint(event.pos):
+                        snake_color = data.GREEN_SNAKE
+                    elif data.ORANGE_SNAKE_BOX.collidepoint(event.pos):
+                        snake_color = data.ORANGE_SNAKE
                 if event.type == pg.KEYDOWN:
                     if event.key == K_ESCAPE:
                         if wanna_quit():
                             sys.exit()
                         else:
                             pg.mouse.set_visible(True)
-                    elif active:
+                    elif name_input_active:
                         if event.key == pg.K_RETURN:
-                            active = False
-                            color = GREEN
+                            name_input_active = False
+                            name_input_color = data.GREEN
                         elif event.key == pg.K_BACKSPACE:
                             name = name[:-1]
                         else:
                             name += event.unicode
 
-            if snake_color == GREEN_SNAKE:
-                pg.draw.rect(SCREEN, BLUE, green_snake_box, 2)
-                pg.draw.rect(SCREEN, BLACK, orange_snake_box, 2)
+            if snake_color == data.GREEN_SNAKE:
+                pg.draw.rect(SCREEN, data.BLUE, data.GREEN_SNAKE_BOX, 2)
+                pg.draw.rect(SCREEN, data.BLACK, data.ORANGE_SNAKE_BOX, 2)
             else:
-                pg.draw.rect(SCREEN, BLACK, green_snake_box, 2)
-                pg.draw.rect(SCREEN, BLUE, orange_snake_box, 2)
+                pg.draw.rect(SCREEN, data.BLACK, data.GREEN_SNAKE_BOX, 2)
+                pg.draw.rect(SCREEN, data.BLUE, data.ORANGE_SNAKE_BOX, 2)
 
-            name_width = FONT.size(name)[0]
+            name_width = data.FONT.size(name)[0]
             # set w and h larger to clear old borders and name
-            name_box.w += 5
-            name_box.h += 5
-            draw_text_box('', name_box, BLACK, BLACK)
-            name_box.h -= 5
-            # set name_box width to match name_width
-            name_box.w = max(100, name_width+15)
-            name_box.x = 300 - name_box.w/2
-            # draw name in name_box
-            draw_text_box(name, name_box, RED, color, 2)
+            data.NAME_BOX.w += 5
+            data.NAME_BOX.h += 5
+            draw_text_box('', data.NAME_BOX, data.BLACK, data.BLACK)
+            data.NAME_BOX.h -= 5
+            # set data.NAME_BOX width to match name_width
+            data.NAME_BOX.w = max(100, name_width+15)
+            data.NAME_BOX.x = 300 - data.NAME_BOX.w/2
+            # draw name in data.NAME_BOX
+            draw_text_box(name, data.NAME_BOX, data.RED, name_input_color, 2)
 
             pg.display.update()
             pg.time.delay(30)
@@ -147,13 +135,13 @@ class Game:
             bottom -= i*i / 2
             i += 1
 
-        SCREEN.fill(BLACK)
-        pg.draw.rect(SCREEN, GREY, STATUS_BAR)
-        text_surface = SMALL_FONT.render(name, True, RED)
+        SCREEN.fill(data.BLACK)
+        pg.draw.rect(SCREEN, data.GREY, STATUS_BAR)
+        text_surface = data.SMALL_FONT.render(name, True, data.RED)
         SCREEN.blit(text_surface, (10, 605))
-        draw_text_box('small:0', Rect(580, 600, 0, 0), RED, GREY)
+        draw_text_box('small:0', Rect(580, 600, 0, 0), data.RED, data.GREY)
         for wall in self.walls:
-            SCREEN.blit(BRICK, wall)
+            SCREEN.blit(data.BRICK, wall)
 
         pg.mouse.set_visible(False)
         return name, snake_color
@@ -162,7 +150,7 @@ class Game:
         """ Pauses game when p is pressed during play. Game continues when p is pressed again. """
 
         org_screen = SCREEN.copy()
-        draw_text_box('title:PAUSED', Rect(300, 275, 0, 0), RED)
+        draw_text_box('title:PAUSED', Rect(300, 275, 0, 0), data.RED)
         pg.display.update()
         while True:
             for event in pg.event.get():
@@ -190,23 +178,23 @@ class Game:
         global food_position, food_is_super
         food_is_super = False
 
-        while True: # runs until the random spot is free of snake
+        while True:  # runs until the random spot is free of snake
             food_position = (randint(0, 29) * 20, randint(0, 29) * 20)
             if food_position not in player.snake + game.walls:
-                # 1/20 chance for placed food to be SUPERFOOD
+                # 1/20 chance for placed food to be data.SUPERFOOD
                 # which speeds up snake when eaten
                 if randint(0, 20) == 0:
-                    SCREEN.blit(SUPERFOOD, food_position)
+                    SCREEN.blit(data.SUPERFOOD, food_position)
                     food_is_super = True
                 else:
-                    SCREEN.blit(food, food_position)
+                    SCREEN.blit(data.FOOD, food_position)
                 break
 
     def change_level(self, level):
         """ clears SCREEN, cuts snake, moves it to start and draws new walls. """
 
         # clear SCREEN
-        draw_text_box('', Rect(0, 0, self.width, self.height), YELLOW, BLACK)
+        draw_text_box('', Rect(0, 0, self.width, self.height), data.YELLOW, data.BLACK)
 
         # move snake to start
         player.snake = [(140, 100), (160, 100), (180, 100), (200, 100)]
@@ -214,22 +202,22 @@ class Game:
             SCREEN.blit(player.img, position)
 
         # new walls
-        self.walls = snake_data.levels[level % len(snake_data.levels)]
+        self.walls = data.levels[level % len(data.levels)]
         for wall in self.walls:
-            SCREEN.blit(BRICK, wall)
+            SCREEN.blit(data.BRICK, wall)
 
         # replace food if it's on new walls or snake
         if food_position in player.snake:
             SCREEN.blit(player.img, food_position)
             self.place_food()
         elif food_position in self.walls:
-            SCREEN.blit(BRICK, food_position)
+            SCREEN.blit(data.BRICK, food_position)
             self.place_food()
         else:
-            SCREEN.blit(food, food_position)
+            SCREEN.blit(data.FOOD, food_position)
 
         # change level number and update SCREEN
-        draw_text_box('small:' + str(player.lvl), Rect(560, 600, 40, 30), RED, GREY)
+        draw_text_box('small:' + str(player.lvl), Rect(560, 600, 40, 30), data.RED, data.GREY)
         pg.display.update()
 
         # reset snake direction and speed
@@ -256,7 +244,7 @@ class Player:
     def move(self):
         """ Processes players actions during game loop """
 
-        score_box = Rect(SMALL_FONT.size(name)[0] + 20, 600, 100, 30)
+        score_box = Rect(data.SMALL_FONT.size(name)[0] + 20, 600, 100, 30)
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
@@ -296,7 +284,7 @@ class Player:
                          (self.snake[-1][1] + self.vy) % game.height)
 
         if head_position == food_position:
-            CHOMP.play()
+            data.CHOMP.play()
             if food_is_super:
                 game.speed = 8
             elif game.speed > 6:
@@ -304,46 +292,46 @@ class Player:
             game.score += 20
             game.place_food()
         elif head_position in self.snake + game.walls:
-            UGH.play()
+            data.UGH.play()
             pg.time.delay(1500)
             game.playing = False
             update_highscores()
             return
         else:
             # removes tail if no food was eaten
-            SCREEN.blit(back, player.snake.pop(0))
+            SCREEN.blit(data.BACK, player.snake.pop(0))
             if food_is_super:
-                SCREEN.blit(SUPERFOOD, food_position)
+                SCREEN.blit(data.SUPERFOOD, food_position)
             else:
-                SCREEN.blit(food, food_position)
+                SCREEN.blit(data.FOOD, food_position)
 
         self.snake.append(head_position)
         SCREEN.blit(self.img, head_position)
 
         # draws game.score on STATUS_BAR
-        pg.draw.rect(SCREEN, GREY, score_box)
-        text_surface = SMALL_FONT.render(str(game.score), True, RED)
+        pg.draw.rect(SCREEN, data.GREY, score_box)
+        text_surface = data.SMALL_FONT.render(str(game.score), True, data.RED)
         SCREEN.blit(text_surface, (score_box.x, 605))
         pg.display.update()
 
         if game.score >= (self.lvl + 1) * 200:
-            draw_text_box('title:LVL UP!', Rect(300, 275, 0, 0), GREEN)
+            draw_text_box('title:LVL UP!', Rect(300, 275, 0, 0), data.GREEN)
             pg.display.update()
-            VICTORY.play()
+            data.VICTORY.play()
             pg.time.delay(2000)
             self.lvl += 1
             game.change_level(self.lvl)
 
 
-def draw_text_box(text, box, text_color=YELLOW, box_color=GREEN, border=0):
+def draw_text_box(text, box, text_color=data.YELLOW, box_color=data.GREEN, border=0):
     """ Draws box with horizontally centered text. Top of text at box.y + 5 """
 
     if text[0:6] == 'title:':
-        text_surface = TITLE_FONT.render(text[6:], True, text_color)
+        text_surface = data.TITLE_FONT.render(text[6:], True, text_color)
     elif text[0:6] == 'small:':
-        text_surface = SMALL_FONT.render(text[6:], True, text_color)
+        text_surface = data.SMALL_FONT.render(text[6:], True, text_color)
     else:
-        text_surface = FONT.render(text, True, text_color)
+        text_surface = data.FONT.render(text, True, text_color)
 
     if box.w > 0 and box.h > 0:
         pg.draw.rect(SCREEN, box_color, box, border)
@@ -359,7 +347,7 @@ def wanna_quit():
     back_box = Rect(200, 200, 200, 160)
     quit_box = Rect(250, 260, 100, 35)
     continue_box = Rect(250, 305, 100, 35)
-    draw_text_box('Wanna quit?', back_box, YELLOW, BLUE)
+    draw_text_box('Wanna quit?', back_box, data.YELLOW, data.BLUE)
     draw_text_box('Yes', quit_box)
     draw_text_box('No', continue_box)
     pg.display.update()
@@ -397,7 +385,7 @@ def update_highscores():
 
 
 if __name__ == '__main__':
-    MUSIC.play(-1)
+    data.MUSIC.play(-1)
     while True:
         game = Game()
         player = Player(*game.main())
--- snake_data.py
+++ snake_data.py
@@ -1,31 +1,79 @@
+# ------------------------------------ #
+# LEVEL AND CONSTANT DATA FOR SNAKE 42 #
+# ------------------------------------ #
+# field is
+#
+#
+#
+#
 
-## ----------------------- ##
-## LEVEL DATA FOR SNAKE 42 ##
-## ----------------------- ##
-
+import os
+import pygame as pg
 from random import randint
+from pygame.locals import *
+
+# Colors
+GREEN = (13, 89, 28)
+LIGHTGREEN = (34, 177, 76)
+ORANGE = (255, 127, 39)
+GREY = (160, 163, 165)
+YELLOW = (229, 212, 57)
+RED = (239, 14, 14)
+BLUE = (72, 92, 242)
+BLACK = (0, 0, 0)
+
+pg.init()
+
+# Images and sounds
+os.chdir("media")
+BACK = pg.image.load('back.png')  # black 20x20 square, used for resetting
+FOOD = pg.image.load('food.png')
+SUPERFOOD = pg.image.load('SUPERFOOD.png')
+GREEN_SNAKE = pg.image.load('snake1.png')
+ORANGE_SNAKE = pg.image.load('snake2.png')
+BRICK = pg.image.load('BRICK.png')
+SMALL_FONT = pg.font.Font('kindergarten.ttf', 20)
+FONT = pg.font.Font('kindergarten.ttf', 32)
+TITLE_FONT = pg.font.Font('kindergarten.ttf', 50)
+CHOMP = pg.mixer.Sound('CHOMP.wav')
+VICTORY = pg.mixer.Sound('VICTORY.wav')
+MUSIC = pg.mixer.Sound('MUSIC.wav')
+UGH = pg.mixer.Sound('UGH.wav')
+MUSIC.set_volume(0.45)
+os.chdir("..")
+
+# Rects
+START_BOX = Rect(250, 200, 100, 35)
+NAME_BOX = Rect(250, 300, 100, 40)
+GREEN_SNAKE_BOX = Rect(260, 400, 30, 30)
+ORANGE_SNAKE_BOX = Rect(310, 400, 30, 30)
+MUSIC_BOX = Rect(226, 470, 152, 35)
 
 levels = []
 
-def random_level(n):
-	'''creates a level with n randomly placed walls'''
-	level = []
-	while len(level) < n:
-		wall = (randint(0,14) * 40, randint(0,29) * 20)
-		if wall in [(140,100),(160,100),(180,100),(200,100),(220,100),(240,100)]:
-			continue
-		else:
-			level.append(wall)
-	return level
 
+def random_level(n):
+    """ Creates a level with n randomly placed walls """
+    level = []
+    while len(level) < n:
+        wall = (randint(0, 14) * 40, randint(0, 29) * 20)
+        if wall in [(140, 100), (160, 100), (180, 100), (200, 100), (220, 100), (240, 100)]:
+            continue
+        else:
+            level.append(wall)
+    return level
+
+
+# empty field
 _0 = []
 levels.append(_0)
 
-_1 = 	[(i, 200) for i in range(100, 520, 20)] + [(i, 400) for i in range(100, 520, 20)]
+# horizontal bars in middle
+_1 = [(i, 200) for i in range(100, 520, 20)] + [(i, 400) for i in range(100, 520, 20)]
 levels.append(_1)
 
 # walls around field
-_2 =  [(i, 0) for i in range(0, 600, 20)] + [(i, 580) for i in range(0, 600, 20)]
+_2 = [(i, 0) for i in range(0, 600, 20)] + [(i, 580) for i in range(0, 600, 20)]
 _2 += [(0, i) for i in range(0, 600, 20)] + [(580, i) for i in range(0, 600, 20)]
 levels.append(_2)
 
@@ -43,8 +91,7 @@ levels.append(_6)
 
 _7 = []
 
-
 if __name__ == '__main__':
-	for lvl in levels:
-		# print(lvl)
-		pass
+    for lvl in levels:
+        # print(lvl)
+        pass
